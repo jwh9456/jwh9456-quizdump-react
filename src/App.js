28e4bc289907539fcd280899830066035468c7c8
jwh9456 <jwh9456@naver.com>
2023-10-27 16:14:22 +0900

    d

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [isSubmitted, setIsSubmitted] = useState(false);
   const [isCorrect, setIsCorrect] = useState(false);
   const [selected, setSelected] = useState([]);
-  const [currentQuestion, setCurrentQuestion] = useState(1);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
   const [score, setScore] = useState(0);
   const [answers, setAnswers] = useState("");
   const q = data[currentQuestion];
@@ -15,6 +15,7 @@ function App() {
     setIsSubmitted(false);
     setIsCorrect(false);
     setAnswers("");
+    setSelected([]);
   }, [currentQuestion]);
 
   return (
@@ -22,15 +23,30 @@ function App() {
       <p>QuizDump</p>
 
       <div>
-        <p>현재 문제: {currentQuestion}</p>
+        <p>현재 문제: <textarea value={currentQuestion} onChange={
+          (e) => {
+            if (parseInt(e.target.value) <= 0 || parseInt(e.target.value) >= data.length || isNaN(parseInt(e.target.value))) {
+              alert("범위를 벗어난 문제입니다.")
+            }
+            else setCurrentQuestion(parseInt(e.target.value))
+          }
+
+        }></textarea> </p>
         <div>
           <p>{q.question}</p>
           {q.choices.map((c, index) => {
+            const isChecked = selected.includes(c.option);
             return (
-              <div key={index} onClick={()=> {
-                setSelected([...selected,c.option])}
-              }>
-                <p>{c.option} : {c.text}</p>
+              <div key={index} onClick={() => {
+                if (isChecked) {
+                  setSelected(selected.filter((option) => option !== c.option));
+                } else {
+                  setSelected([...selected, c.option]);
+                }
+              }}>
+                <p>
+                  <input type="checkbox" checked={isChecked} /> {c.option} : {c.text}
+                </p>
               </div>
             );
           })}
@@ -39,6 +55,11 @@ function App() {
         <div>
           <button
             onClick={() => {
+              if (selected.length === 0){
+                alert("답을 선택해주세요.");
+                return;
+              }
+
               setIsSubmitted(true);
               setIsCorrect(q.answer === answers);
               setScore(isCorrect ? score + 1 : score);
@@ -51,12 +72,8 @@ function App() {
             {isSubmitted && !isCorrect && <p>오답입니다!</p>}
           </div>
 
-          <button onClick={() => setCurrentQuestion(currentQuestion - 1)}>
-            이전 문제
-          </button>
-          <button onClick={() => setCurrentQuestion(currentQuestion + 1)}>
-            다음 문제
-          </button>
+          {currentQuestion !== 0 && <button onClick={() => setCurrentQuestion(currentQuestion - 1)}> 이전 문제</button>}
+          {currentQuestion <= data.length && <button onClick={() => setCurrentQuestion(currentQuestion + 1)}> 다음 문제</button>}
         </div>
       </div>
 
